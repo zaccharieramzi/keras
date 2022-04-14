a5c755b2bab576b2dcc5e2a79c764c4b5602a1f4
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-14 11:46:03 +0200

    done with torch tf model comparison

--- keras/personal_scripts/torch_tf_model_comparison.py
+++ keras/personal_scripts/torch_tf_model_comparison.py
@@ -42,8 +42,13 @@ def basic_block(x, filters, stride=1, use_bias=True, conv_shortcut=True,
     else:
         shortcut = x
 
+    if stride > 1:
+        x = layers.ZeroPadding2D(padding=((1,0),(1,0)), name=name + '_1_pad')(x)
+        padding_mode = 'valid'
+    else:
+        padding_mode = 'same'
     x = layers.Conv2D(
-        filters, kernel_size, padding='SAME', strides=stride,
+        filters, kernel_size, padding=padding_mode, strides=stride,
         use_bias=use_bias,
         name=name + '_1_conv')(x)
     x = layers.BatchNormalization(
@@ -240,89 +245,8 @@ for torch_model, tf_model in zip(
     torch_input = torch.from_numpy(np.transpose(example_input, (0, 3, 1, 2))).float()
     tf_output = tf_model(tf_input, training=False)
     torch_output = torch.softmax(torch_model(torch_input), dim=1)
-
-    #### Unique conv testing #####
-    example_input = np.random.normal(size=(1, 224, 224, 64))
-    tf_input = tf.constant(example_input, dtype=tf.float32)
-    torch_input = torch.from_numpy(np.transpose(example_input, (0, 3, 1, 2))).float()
-    torch_conv = torch_model.layer2[0].conv1
-    tf_conv = tf_model.get_layer('conv3_block1_1_conv')
-    tf_conv_output = tf_conv(tf_input, training=False)
-    torch_conv_output = torch_conv(torch_input)
-    np.testing.assert_equal(
-        tf_conv.get_weights()[1],
-        np.zeros_like(tf_conv.get_weights()[1]),
-    )
-    print('bias is 0')
     np.testing.assert_almost_equal(
-        np.transpose(tf_conv.get_weights()[0], (3, 2, 0, 1)),
-        torch_conv.weight.data.numpy(),
-    )
-    print('weights are the same')
-    import pdb; pdb.set_trace()
-    np.testing.assert_almost_equal(
-        np.transpose(tf_conv_output.numpy(), (0, 3, 1, 2)),
-        torch_conv_output.detach().numpy(),
-    )
-
-
-
-
-    def torch_inter_model(inputs):
-        x = torch_model.conv1(inputs)
-        x = torch_model.bn1(x)
-        x = torch_model.relu(x)
-        x = torch_model.maxpool(x)
-
-        x = torch_model.layer1(x)
-        x = torch_model.layer2[0].conv1(x)
-        # x = torch_model.layer3(x)
-        # x = torch_model.layer4(x)
-
-        # x = torch_model.avgpool(x)
-        # x = torch.flatten(x, 1)
-        # x = torch_model.fc(x)
-
-        return x
-    torch_inter_output = torch_inter_model(torch_input)
-    if tf_model.name == 'resnet34':
-        import ipdb; ipdb.set_trace()
-    tf_inter_model = tf.keras.Model(
-        tf_model.input,
-        # for resnet18, layer 1 is good, as well as 1st downsampling in layer 2
-        # however 1st conv in layer 2 is not working
-        # ['input_1', 'conv1_pad', 'conv1_conv', 'conv1_bn', 'conv1_relu', 'pool1_pad', 'pool1_pool',
-        # 'conv2_block1_1_conv', 'conv2_block1_1_bn', 'conv2_block1_1_relu', 'conv2_block1_2_conv',
-        # 'conv2_block1_2_bn', 'conv2_block1_add', 'conv2_block1_out', 'conv2_block2_1_conv',
-        # 'conv2_block2_1_bn', 'conv2_block2_1_relu', 'conv2_block2_2_conv', 'conv2_block2_2_bn',
-        # 'conv2_block2_add', 'conv2_block2_out',
-        # 'conv3_block1_1_conv', 'conv3_block1_1_bn',
-        # 'conv3_block1_1_relu', 'conv3_block1_0_conv', 'conv3_block1_2_conv', 'conv3_block1_0_bn',
-        # 'conv3_block1_2_bn', 'conv3_block1_add', 'conv3_block1_out', 'conv3_block2_1_conv',
-        # 'conv3_block2_1_bn', 'conv3_block2_1_relu', 'conv3_block2_2_conv', 'conv3_block2_2_bn',
-        # 'conv3_block2_add', 'conv3_block2_out',
-        # 'conv4_block1_1_conv', 'conv4_block1_1_bn',
-        # 'conv4_block1_1_relu', 'conv4_block1_0_conv', 'conv4_block1_2_conv', 'conv4_block1_0_bn',
-        # 'conv4_block1_2_bn', 'conv4_block1_add', 'conv4_block1_out', 'conv4_block2_1_conv',
-        # 'conv4_block2_1_bn', 'conv4_block2_1_relu', 'conv4_block2_2_conv', 'conv4_block2_2_bn',
-        # 'conv4_block2_add', 'conv4_block2_out',
-        # 'conv5_block1_1_conv', 'conv5_block1_1_bn',
-        # 'conv5_block1_1_relu', 'conv5_block1_0_conv', 'conv5_block1_2_conv', 'conv5_block1_0_bn',
-        # 'conv5_block1_2_bn', 'conv5_block1_add', 'conv5_block1_out', 'conv5_block2_1_conv',
-        # 'conv5_block2_1_bn', 'conv5_block2_1_relu', 'conv5_block2_2_conv', 'conv5_block2_2_bn',
-        # 'conv5_block2_add', 'conv5_block2_out',
-        # 'avg_pool', 'predictions']
-        tf_model.get_layer('conv3_block1_1_conv').output if tf_model.name == 'resnet18' else tf_model.get_layer('conv2_block3_out').output,
-    )
-    import ipdb; ipdb.set_trace()
-    tf_inter_output = tf_inter_model(tf_input, training=False)
-    np_tf_output = np.transpose(tf_inter_output.numpy(), (0, 3, 1, 2))
-    np_torch_output = torch_inter_output.detach().numpy()
-    np.testing.assert_almost_equal(
-        np_tf_output,
-        np_torch_output,
+        tf_output.numpy(),
+        torch_output.detach().numpy(),
         decimal=5,
     )
-    print(f'done {tf_model.name}')
-    # np.testing.assert_almost_equal(tf_output.numpy(), torch_output.detach().numpy())
-    break
